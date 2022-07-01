e41c984c39375056c166c14ff1f70fd71b3d17b5
timothyoh777 <timothyoh777@gmail.com>
2022-06-30 23:48:05 -0500

    more work done

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,57 @@
+const bcrypt = require('bcryptjs');
+const jwt = require ('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+
+const { validateRegisterInput } = require('../../util/validators');
+const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
 module.exports = {
     Mutation: {
-        register(_, args, context, info){
+        async register(_,
+            { 
+                registerInput : { username, email, password, confirmPassword }
+            },
+            
+        ) {
             // TODO: Validate user data
-            // TODO: Make sure user doesnt already exist
+            const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+            if(!valid){
+                throw new UserInputError('Errors', { errors });
+            }
+            
+            const user = await User.findOne({ username });
+            if(user){
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+
             // TODO: hash password and create auth token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
         }
     }
 }
