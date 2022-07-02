f408b4b05a760f02eb3f9713b48dd58e4c435ba2
timothyoh777 <timothyoh777@gmail.com>
2022-07-01 21:01:51 -0500

    authentication for posts, still not working!

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,5 @@
 const Post = require('../../models/Post');
-
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
@@ -10,6 +10,37 @@ module.exports = {
             } catch(err){
                 throw new Error(err);
             }
+        },
+        async getPost(_, { postId }){
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }                
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
+    },
+    Mutation : {
+        async createPost(_, { body }, context){
+            const user = checkAuth(context);
+            if (body.trim() === '') {
+                throw new Error('Post body must not be empty');
+              }
+
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
         }
     }
-}
+};
--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -82,7 +82,7 @@ module.exports = {
                 ...res._doc,
                 id: res._id,
                 token
-            }
-        }
+            };
+        },
     }
-}
+};
